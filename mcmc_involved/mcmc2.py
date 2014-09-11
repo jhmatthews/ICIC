3a6c7e30e7ed9090bc344a7e6f52b4aea74f7a2d
jhmatthews <jimim@hotmail.co.uk>
2014-09-11 13:32:43 +0100

    parallelised code

--- mcmc_involved/mcmc2.py
+++ mcmc_involved/mcmc2.py
@@ -4,7 +4,8 @@ import numpy as np
 import math 
 import scipy 
 import mcmc_sub as sub
-import time
+import time, sys
+from mpi4py import MPI
 
 def log_normal_gaussian(x, mu, sigma):
 
@@ -15,6 +16,12 @@ def log_normal_gaussian(x, mu, sigma):
 	return np.log(1.0 / np.sqrt(2.0 * np.pi) / sigma) + y
 
 
+def Log(print_string):
+
+	if my_rank == 0:
+		print print_string
+
+	return 0
 
 def do_general_mc (start_params, NSAMPLES, data, f_likelihood, sigmas, prior = None, param_limits=None, return_accept=False):
 
@@ -69,6 +76,16 @@ def do_general_mc (start_params, NSAMPLES, data, f_likelihood, sigmas, prior = N
 
 	for i in range(1,NSAMPLES):
 
+		if (float(i) % 100) == 0:
+			tnow = time.time()
+			eta = (NSAMPLES - i) * (tnow - t0) / (float(i))
+			Log("Number %i, time elapsed %.2fs, eta %.2fs" % (i, tnow - t0, eta) )
+			Log("Acceptance %.2fpc" % (float(accept) / i))
+			#print "Thread %i: %i, time elapsed %.2fs, eta %.2fs" % (my_rank, i, tnow - t0, eta)
+			#sys.stdout.write("Number %i, time elapsed %.2fs, eta %.2fs" % (i, tnow - t0, eta))
+			#sys.stdout.write("Acceptance %.2fpc\n" % (float(accept) / i))
+			#sys.stdout.flush()
+
 
 		# generate trial points from our proposal distribution
 		# change this
@@ -103,7 +120,6 @@ def do_general_mc (start_params, NSAMPLES, data, f_likelihood, sigmas, prior = N
 	ps = np.e ** ps
 
 	tnew = time.time()
-	print tnew - t0
 
 	if return_accept:
 		return params, ps, np.float(accept) / NSAMPLES
@@ -111,7 +127,7 @@ def do_general_mc (start_params, NSAMPLES, data, f_likelihood, sigmas, prior = N
 		return params, ps
 
 
-def make_plot(params, ps, fname, labels, lims, nbins = 100):
+def make_plot(params, ps, fname, labels, lims, twodhist=p.scatter, nbins = 100):
 
 	'''
 	make a triangular shaped plot, with 2D probability
@@ -141,16 +157,13 @@ def make_plot(params, ps, fname, labels, lims, nbins = 100):
 			iy = i
 
 			p.subplot(nparams, nparams, i_2d)
-			p.scatter(params[ix], params[iy], c=ps, edgecolors="None")
+			twodhist(params[ix], params[iy], c=ps, edgecolors="None")
 
 			p.xlim(lims[ix])
 			p.ylim(lims[iy])
 
 			p.xlabel(labels[ix])
 			p.ylabel(labels[iy])
-
-			print "subplot:", nparams, nparams, i_2d 
-			print "x:", labels[ix], "y:", labels[iy]  
 
 	p.savefig(fname)
 	p.clf()
@@ -252,14 +265,44 @@ def p_accept_curve(svals = np.arange(-5,0.5,0.1)):
 
 	return 0
 
+t0 = time.time()
+nproc = MPI.COMM_WORLD.Get_size()   	# number of processes
+my_rank = MPI.COMM_WORLD.Get_rank()   	# The number/rank of this process
+my_node = MPI.Get_processor_name()    	# Node where this MPI process runs
+
+NWALKERS = nproc 						# number of walkers in total									# total number of models to run	
+n_walkers = NWALKERS / nproc					# number of walkers for each thread
+remainder = NWALKERS - ( n_walkers * nproc )	# the remainder. e.g. your number of models may 
+
+NSAMPLES = int(sys.argv[1])							# total number of samples 
+nsamples = NSAMPLES / NWALKERS			# number of samples per walker
+
+# little trick to spread remainder out among threads
+# if say you had 19 total walkers, and 4 threads
+# then n_models = 4, and you have 3 remainder
+# this little loop would distribute these three 
+if remainder < my_rank + 1:
+	my_extra = 0
+	extra_below = remainder
+else:
+	my_extra = 1
+	extra_below = my_rank
+
+# where to start and end your loops for each thread
+my_nmin = (my_rank * n_walkers) + extra_below
+my_nmax = my_nmin + n_walkers + my_extra
+
+# total number you actually do
+ndo = my_nmax - my_nmin
+print "This is thread %i with walkers %i to %i, chain lengths %i" % (my_rank, my_nmin, my_nmax, nsamples)	
+
+
+# set barrier so print output doesn't look muddled
+# just waits for other thread
+MPI.COMM_WORLD.Barrier()
 
 data = sub.read_SN()	# read data, len 3 array
 
-omega_start = 0.5
-h_start = 0.5
-NSAMPLES = 10000
-sigmas = [0.01,0.01]
-starts = np.array([omega_start, h_start])
 
 # do first MC and plot up
 # params, ps = do_general_mc (starts, NSAMPLES, data, log_likelihood_chi2, sigmas)
@@ -273,12 +316,52 @@ starts = np.array([omega_start, h_start])
 # params, ps = do_general_mc (starts, NSAMPLES, data, log_likelihood_chi2, sigmas, prior=h_prior_narrow)
 # make_plot(params, ps, "posterior3_withnarrowprior.png", labels=["$\Omega_m$", "$h$"], lims=[(0,1), (0,1)])
 
-
 # p_accept_curve()
 # do first MC and plot up
-starts = np.array([0.45, 0.9, 0.65])
-sigmas = [0.01, 0.01, 0.01]
-params, ps = do_general_mc (starts, 10000, data, log_likelihood_chi2_nonflat, sigmas)
-make_plot(params, ps, "posterior1.png", labels=["$\Omega_M$", "$\Omega_V$", "$h$"], lims=[(0.,1), (0.,1), (0.6,0.66)])
+
+all_params = np.empty((0,3))
+all_ps = np.empty((0))
+
+for i in range( my_nmin, my_nmax):
+	starts = np.array([0.45 + 0.1*np.random.random(), 0.9+0.1*np.random.random(), 0.65+0.1*np.random.random()])
+	sigmas = [0.01, 0.01, 0.01]
+	params, ps = do_general_mc (starts, nsamples, data, log_likelihood_chi2_nonflat, sigmas)
+
+	all_params = np.concatenate((all_params, params.T))
+	all_ps = np.concatenate((all_ps, ps.T))
+
+all_params = all_params.T
+all_ps = all_ps.T
+
+Log('Waiting for threads to finish...')
+
+# set barrier so print output doesn't look muddled
+# just waits for other thread
+MPI.COMM_WORLD.Barrier()
+
+
+
+xps = MPI.COMM_WORLD.gather(all_ps, root=0)
+xparams = MPI.COMM_WORLD.gather(all_params, root=0)
+
+if my_rank == 0:
+	print "gathering chains from other threads"
+
+	all_pdata = np.zeros(NSAMPLES)
+	all_paramdata = np.zeros( (3, NSAMPLES) )
+
+	print "Data gathered." 
+
+	for i in range(nproc):
+		all_pdata[i * n_walkers * nsamples: (i+1) * n_walkers * nsamples] = xps[i]
+		all_paramdata[:,i * n_walkers * nsamples: (i+1) * n_walkers * nsamples] = xparams[i]
+
+	print "Shape of param array ", all_paramdata.shape
+	make_plot(params, ps, "posterior_NSAMPLES%i.png" % NSAMPLES, labels=["$\Omega_M$", "$\Omega_V$", "$h$"], lims=[(0.3,0.7), (0.7,1.5), (0.6,0.7)])
+
+
+print "Total Time Elapsed %.2fs" % (time.time() - t0)
+MPI.COMM_WORLD.Finalize()
+
 
 
